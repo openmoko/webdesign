f4aeba67fec17c2aac49dee842413d9f2b2a03f2
Harald Welte <laforge@openmoko.org>
2007-02-12 15:37:33 +0000

    this is the current default gforge theming directory

---
+++ projects.openmoko.org/usr_share_gforge_www_themes/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * GForge Web Theme Control Page
+ *
+ * Copyright 2002 (c) GFORGE LLC
+ *
+ * @version   $Id: index.php,v 1.9 2004/03/25 23:24:06 tperdue Exp $
+ */
+
+
+Header("Location: /account/");
+
+?>
