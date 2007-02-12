bdcade19d9ca1faac48133ecdb42d71240743470
(no author) <(no author)>
2007-02-12 16:10:27 +0000

    2007-02-11 version from web designers

--- planet.openmoko.org/space_www_planet.openmoko.org/_sifr/init_sifr.js
+++
@@ -1,5 +0,0 @@
-if(typeof sIFR == "function"){
-	sIFR.replaceElement("h2.sifr_headline", named({sFlashSrc: "./_sifr/sansasoft_semibold.swf", sColor: "#333333"}));
-//	sIFR.replaceElement("h3.sifr_headline", named({sFlashSrc: "./_sifr/sansasoft_semibold.swf", sColor: "#ff6600",sHoverColor: "#333333"}));
-	sIFR.debug();
-};
