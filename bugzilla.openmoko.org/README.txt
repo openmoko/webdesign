f1cfbc12047932c6c4ce2ca7aa8050483b4df7ad
Harald Welte <laforge@openmoko.org>
2007-02-12 15:31:04 +0000

    import initial content for webdesign svn trunk

---
+++ bugzilla.openmoko.org/README.txt
@@ -0,0 +1,10 @@
+usr_share_bugzilla_template_en_default  
+
+	This is the default template pre-installed from the Debian bugzilla
+	package
+
+var_lib_bugzilla_template_en_openmoko
+
+	This is our openmoko theme.  As a start, I've populated it with a copy
+	original defalt template
+
---
+++ planet.openmoko.org/space_www_planet.openmoko.org/_sifr/init_sifr.js
@@ -0,0 +1,5 @@
+if(typeof sIFR == "function"){
+	sIFR.replaceElement("h2.sifr_headline", named({sFlashSrc: "./_sifr/sansasoft_semibold.swf", sColor: "#333333"}));
+//	sIFR.replaceElement("h3.sifr_headline", named({sFlashSrc: "./_sifr/sansasoft_semibold.swf", sColor: "#ff6600",sHoverColor: "#333333"}));
+	sIFR.debug();
+};
