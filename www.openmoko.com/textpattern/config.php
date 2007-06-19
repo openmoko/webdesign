7ce8f5c07661a2f4827f3358e664c1dba2637017
Joachim Steiger <roh@openmoko.org>
2007-06-19 02:07:57 +0000

    remove php version

--- www.openmoko.com/textpattern/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$txpcfg['db'] = OPENMOKO_WEB_DB_NAME;
-	$txpcfg['user'] = OPENMOKO_WEB_DB_USER;
-	$txpcfg['pass'] = OPENMOKO_WEB_DB_PASSWD;
-	$txpcfg['host'] = OPENMOKO_WEB_DB_HOST;
-	$txpcfg['table_prefix'] = 'openmoko';
-	$txpcfg['txpath'] = OPENMOKO_WEB_FS_PATH.'/textpattern';
-	$txpcfg['dbcharset'] = 'utf8';
-?>
