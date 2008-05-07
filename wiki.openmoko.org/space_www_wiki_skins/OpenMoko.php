74c69feeb1e9b0cb821dca002faa2fff990b4219
gismo <gismo@openmoko.org>
2008-05-07 08:05:25 +0000

    add google analytics code

--- wiki.openmoko.org/space_www_wiki_skins/OpenMoko.php
+++ wiki.openmoko.org/space_www_wiki_skins/OpenMoko.php
@@ -283,6 +283,14 @@ class OpenMokoTemplate extends QuickTemplate {
 </div>
 <?php $this->html('reporttime') ?>
 
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-XXXXX-X";
+urchinTracker();
+</script>
+
 </body></html>
 <?php
 	wfRestoreWarnings();
