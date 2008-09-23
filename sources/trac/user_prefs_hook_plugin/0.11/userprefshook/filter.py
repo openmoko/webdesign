cfe92c03c58fc8ed262c6d4e41768cfbd700c6ca
Joachim Steiger <roh@openmoko.org>
2008-09-23 17:53:51 +0000

    nonblocking please

--- sources/trac/user_prefs_hook_plugin/0.11/userprefshook/filter.py
+++ sources/trac/user_prefs_hook_plugin/0.11/userprefshook/filter.py
@@ -9,8 +9,8 @@ class UserPrefsHookModule(Component):
     def pre_process_request(self, req, handler):
         if req.path_info == '/prefs' and req.method == 'POST':
 #            self.log.info('invoking PRE /bin/touch req.path_info = %s req.method = %s req.args = %s' % (req.path_info, req.method, req.args))
-            self.log.info('saving user prefs -> calling hook /var/www/bin/mail_fullname_sync/run.sh');
-            os.system("/var/www/bin/mail_fullname_sync/run.sh ")
+            self.log.info('saving user prefs -> calling hook /var/www/bin/mail_fullname_sync/run.sh &');
+            os.system("/var/www/bin/mail_fullname_sync/run.sh & ")
         return handler
 
     def post_process_request(self, req, template, data, content_type):
